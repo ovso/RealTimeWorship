4716f5805970e156d4be2ea1b9a913547984f3fe
peterjaeho <peter@mybroccoli.co.kr>
2020-04-23 08:04:36 +0900

    Create youtube channel api test class

---
+++ app/src/test/java/io/github/ovso/worship/YoutubeApiTest.kt
@@ -0,0 +1,12 @@
+package io.github.ovso.worship
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class YoutubeApiTest {
+    @Test
+    fun `유투브 채널 정보를 받아오기`() {
+        assertEquals(4, 2 + 2)
+    }
+}
