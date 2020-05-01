749994de555460ff94c0727fefef22c60e7c1195
jaeho <ovsoce@gmail.com>
2020-05-01 22:19:26 +0900

    Parsing success on youtube channel

--- app/src/main/java/io/github/ovso/worship/view/ui/main/MainViewModel.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/main/MainViewModel.kt
@@ -2,9 +2,12 @@ package io.github.ovso.worship.view.ui.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import com.google.gson.JsonElement
 import io.github.ovso.worship.data.TasksRepository
 import io.github.ovso.worship.view.base.DisposableViewModel
 import org.jsoup.Jsoup
+import org.jsoup.parser.Parser
 import timber.log.Timber
 
 class MainViewModel(private val tasksRepository: TasksRepository) : DisposableViewModel() {
@@ -26,11 +29,33 @@ class MainViewModel(private val tasksRepository: TasksRepository) : DisposableVi
             )
         )
 
-        val body =
-            Jsoup.parse("https://www.youtube.com/channel/UC6vNHBFM5VLNF53CKycyNZw/videos").body()
-        println(body)
-    }
+        Thread {
+            val get =
+                Jsoup.connect("https://www.youtube.com/channel/UC6vNHBFM5VLNF53CKycyNZw/videos")
+                    .get()
 
+            /*
+
+            {
+  "items": [{"gridVideoRenderer":
+
+            * */
+            val elementsByTag = get.getElementsByTag("script")
+//            val prefix = "{\"responseContext\""
+            val prefix = "[{\"gridVideoRenderer"
+//            val prefix = "\"items\": [{\"gridVideoRenderer\":"
+            elementsByTag.forEach {
+                if (it.data().contains(prefix)) {
+                    val startIndex = it.data().indexOf(prefix)
+                    val endIndex = it.data().indexOf(",\"continuations\"")
+                    val subSequence = it.data().substring(startIndex, endIndex)
+                    Gson().fromJson(subSequence, JsonElement::class.java)
+                    Timber.d("subSequence = $subSequence")
+                    //"items:[{gridVideoRenderer"
+                }
+            }
+        }.start()
+    }
     fun onClick(id: Int) {
         Timber.i("id = $id")
     }
