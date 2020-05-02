2ad683452c43ff97c0d633c6a6f5ceef4c0e816a
jaeho <ovsoce@gmail.com>
2020-05-02 13:06:07 +0900

    Lint error suppress

--- app/src/test/java/io/github/ovso/worship/RepositoryTest.kt
+++ app/src/test/java/io/github/ovso/worship/RepositoryTest.kt
@@ -1,17 +1,12 @@
+@file:Suppress("NonAsciiCharacters")
+
 package io.github.ovso.worship
 
-import com.google.gson.Gson
 import io.github.ovso.worship.data.network.ServiceLocator
 import io.github.ovso.worship.data.network.model.VideoResponse
-import io.github.ovso.worship.extensions.fromJson
-import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Scheduler
-import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 import org.junit.Test
-import java.lang.StringBuilder
 
 /**
  * Example local unit test, which will execute on the development machine (host).
