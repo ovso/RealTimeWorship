e0052645254120d5373c3f910908a56f4814711b
jaeho <ovsoce@gmail.com>
2020-05-04 00:17:13 +0900

    WIP - change theme

--- app/src/main/java/io/github/ovso/worship/view/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/main/MainActivity.kt
@@ -3,15 +3,24 @@ package io.github.ovso.worship.view.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.github.ovso.worship.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupActionBar()
+/*
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+*/
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar)
+        title = "산정현교회"
     }
 
 }
