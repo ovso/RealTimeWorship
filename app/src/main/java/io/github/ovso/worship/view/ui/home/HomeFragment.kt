c924ed34130fd2b60c2912120ff30edd3146368a
jaeho <ovsoce@gmail.com>
2020-05-18 18:13:49 +0900

    Implement open source license `activity`

--- app/src/main/java/io/github/ovso/worship/view/ui/home/HomeFragment.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/home/HomeFragment.kt
@@ -1,6 +1,9 @@
 package io.github.ovso.worship.view.ui.home
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.fragment.app.viewModels
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import io.github.ovso.worship.R
 import io.github.ovso.worship.databinding.FragmentHomeBinding
 import io.github.ovso.worship.extensions.getViewModelFactory
@@ -9,5 +12,12 @@ import io.github.ovso.worship.view.base.DataBindingFragment
 class HomeFragment : DataBindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     override val viewModel by viewModels<HomeViewModel> { getViewModelFactory() }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.tvHome.setOnClickListener {
+            startActivity(Intent(requireContext(), OssLicensesMenuActivity::class.java))
+        }
+    }
 
 }
