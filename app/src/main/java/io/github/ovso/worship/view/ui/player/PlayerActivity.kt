8e976036a5486adac5e38782278e6ff127ad2170
jaeho <ovsoce@gmail.com>
2020-05-22 00:52:30 +0900

    Apply shimmerFrameLayout

--- app/src/main/java/io/github/ovso/worship/view/ui/player/PlayerActivity.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/player/PlayerActivity.kt
@@ -9,12 +9,5 @@ class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.player_activity)
-/*
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, PlayerFragment.newInstance())
-                .commitNow()
-        }
-*/
     }
 }
--- app/src/main/java/io/github/ovso/worship/view/ui/video/VideoFragment.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/video/VideoFragment.kt
@@ -1,10 +1,13 @@
 package io.github.ovso.worship.view.ui.video
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.facebook.shimmer.ShimmerFrameLayout
 import io.github.ovso.worship.R
 import io.github.ovso.worship.databinding.FragmentMainBinding
 import io.github.ovso.worship.extensions.getViewModelFactory
@@ -22,7 +25,16 @@ class VideoFragment : DataBindingFragment<FragmentMainBinding>(R.layout.fragment
         super.onActivityCreated(savedInstanceState)
         setupRecyclerView()
         observe()
+        startShimmer()
     }
+
+    private fun startShimmer() {
+        val root = binding.root as? ViewGroup
+        val view =
+            LayoutInflater.from(context).inflate(R.layout.view_all_list_shimmer, root, false)
+        root?.addView(view)
+    }
+
 
     private fun setupRecyclerView() {
         rv_main.adapter = adapter
@@ -32,7 +44,15 @@ class VideoFragment : DataBindingFragment<FragmentMainBinding>(R.layout.fragment
     private fun observe() {
         viewModel.getItems().observe(viewLifecycleOwner, Observer {
             adapter.submitList(it)
+            stopShimmer()
         })
+    }
+
+    private fun stopShimmer() {
+        val root = binding.root as? ViewGroup
+        val shimmerFrameLayout = root?.getChildAt(1) as? ShimmerFrameLayout
+        shimmerFrameLayout?.stopShimmer()
+        shimmerFrameLayout?.let { root.removeView(shimmerFrameLayout) }
     }
 
     override fun onDestroy() {
