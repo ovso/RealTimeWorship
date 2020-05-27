a25df79568204413f466ce8827291cf14fc60d91
jaeho <ovsoce@gmail.com>
2020-05-28 00:44:08 +0900

    Safe calls for channelId

--- app/src/main/java/io/github/ovso/worship/view/ui/video/VideoActivity.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/video/VideoActivity.kt
@@ -8,12 +8,14 @@ class VideoActivity : AppCompatActivity(R.layout.activity_video) {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     val channelId = intent.getStringExtra("channel_id")
-    if (savedInstanceState == null && channelId.isNullOrEmpty().not()) {
-      supportFragmentManager.beginTransaction()
-        .replace(
-          R.id.fl_video, VideoFragment.newInstance(channelId),
-          VideoFragment::class.java.name
-        ).commitAllowingStateLoss()
+    if (savedInstanceState == null) {
+      channelId?.let {
+        supportFragmentManager.beginTransaction()
+          .replace(
+            R.id.fl_video, VideoFragment.newInstance(channelId),
+            VideoFragment::class.java.name
+          ).commitAllowingStateLoss()
+      }
     }
   }
 }
