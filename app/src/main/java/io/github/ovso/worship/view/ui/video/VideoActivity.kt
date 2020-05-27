aeec9934443876947b5bb1c4ce141a5ad63817d0
jaeho <ovsoce@gmail.com>
2020-05-28 01:02:52 +0900

    Implement toolbar on VideoActivity

--- app/src/main/java/io/github/ovso/worship/view/ui/video/VideoActivity.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/video/VideoActivity.kt
@@ -1,21 +1,31 @@
 package io.github.ovso.worship.view.ui.video
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import io.github.ovso.worship.R
+import kotlinx.android.synthetic.main.activity_video.*
 
 class VideoActivity : AppCompatActivity(R.layout.activity_video) {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    setSupportActionBar(toolbar)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    supportActionBar?.hide()
     val channelId = intent.getStringExtra("channel_id")
     if (savedInstanceState == null) {
       channelId?.let {
         supportFragmentManager.beginTransaction()
           .replace(
-            R.id.fl_video, VideoFragment.newInstance(channelId),
+            R.id.fl_video_container, VideoFragment.newInstance(channelId),
             VideoFragment::class.java.name
           ).commitAllowingStateLoss()
       }
     }
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    finish()
+    return super.onOptionsItemSelected(item)
   }
 }
