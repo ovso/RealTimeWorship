7314255a5262b46dfbf63ffe2cb18209bb9184cd
jaeho <ovsoce@gmail.com>
2020-05-31 00:06:43 +0900

    Print lifecycle log

--- app/src/main/java/io/github/ovso/worship/view/ui/player/PlayerFragment.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/player/PlayerFragment.kt
@@ -1,12 +1,19 @@
 package io.github.ovso.worship.view.ui.player
 
 import android.app.Dialog
+import android.content.Context
 import android.content.res.Resources
 import android.graphics.Point
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -16,11 +23,15 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.You
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.utils.loadOrCueVideo
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 import io.github.ovso.worship.R
+import io.github.ovso.worship.databinding.ViewPlayerBinding
 import io.github.ovso.worship.extensions.getIndicatorSize
+import io.github.ovso.worship.extensions.getViewModelFactory
+import timber.log.Timber
 
 
 class PlayerFragment private constructor() : BottomSheetDialogFragment() {
   private lateinit var playerView: YouTubePlayerView
+  private val viewModel by viewModels<PlayerViewModel> { getViewModelFactory() }
 
   companion object {
     fun newInstance(videoId: String): PlayerFragment =
@@ -30,21 +41,30 @@ class PlayerFragment private constructor() : BottomSheetDialogFragment() {
   }
 
   private lateinit var behavior: BottomSheetBehavior<View>
+  private lateinit var contentView: View
 
   override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+    Timber.d("Player onCreateDialog")
     val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
-    val view = View.inflate(context, R.layout.view_player, null)
-    val container = view.findViewById<LinearLayout>(R.id.ll_player_container)
+    contentView = View.inflate(context, R.layout.view_player, null)
+    val container = contentView.findViewById<LinearLayout>(R.id.ll_player_container)
     val params = container.layoutParams
     params.height = Resources.getSystem().displayMetrics.heightPixels
     container.layoutParams = params
-    playerView = view.findViewById(R.id.ypv_player)
+    playerView = contentView.findViewById(R.id.ypv_player)
     playerView.addFullScreenListener(fullScreenListener)
     lifecycle.addObserver(playerView)
-    dialog.setContentView(view)
-    behavior = BottomSheetBehavior.from(view.parent as View)
+    dialog.setContentView(contentView)
+    dataBinding(contentView)
+    behavior = BottomSheetBehavior.from(contentView.parent as View)
     playVideo()
     return dialog
+  }
+
+  private fun dataBinding(view: View) {
+    val bind = DataBindingUtil.bind<ViewPlayerBinding>(view)
+    bind?.viewModel = viewModel
+//    bind?.lifecycleOwner = viewLifecycleOwner
   }
 
   private fun switchToLandscapeMode() {
@@ -52,7 +72,7 @@ class PlayerFragment private constructor() : BottomSheetDialogFragment() {
     val screenSizeX = getScreenSize().x
     val screenSizeY = getScreenSize().y
     params.height = screenSizeX
-    params.width = screenSizeY  - requireContext().getIndicatorSize()
+    params.width = screenSizeY - requireContext().getIndicatorSize()
     playerView.layoutParams = params
     playerView.pivotX = (screenSizeX / 2).toFloat()
     playerView.pivotY = (screenSizeX / 2).toFloat()
@@ -67,10 +87,12 @@ class PlayerFragment private constructor() : BottomSheetDialogFragment() {
   private val fullScreenListener = object : YouTubePlayerFullScreenListener {
     override fun onYouTubePlayerEnterFullScreen() {
       switchToLandscapeMode()
+      contentView.findViewById<ImageView>(R.id.iv_player_bookmark).isVisible = false
     }
 
     override fun onYouTubePlayerExitFullScreen() {
       switchToPortraitMode()
+      contentView.findViewById<ImageView>(R.id.iv_player_bookmark).isVisible = true
     }
   }
 
@@ -83,6 +105,7 @@ class PlayerFragment private constructor() : BottomSheetDialogFragment() {
 
   override fun onStart() {
     super.onStart()
+    Timber.d("Player onStart")
     behavior.state = BottomSheetBehavior.STATE_EXPANDED
   }
 
@@ -99,8 +122,48 @@ class PlayerFragment private constructor() : BottomSheetDialogFragment() {
   }
 
   override fun onDestroyView() {
-    playerView.removeFullScreenListener(fullScreenListener)
+
     super.onDestroyView()
+  }
+
+  override fun onAttach(context: Context) {
+    super.onAttach(context)
+    Timber.d("Player onAttach")
+  }
+
+  override fun onResume() {
+    super.onResume()
+    Timber.d("Player onResume")
+  }
+
+  override fun onActivityCreated(savedInstanceState: Bundle?) {
+    super.onActivityCreated(savedInstanceState)
+    Timber.d("Player onActivityCreated deprecated")
+  }
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    Timber.d("Player onCreateView")
+    return super.onCreateView(inflater, container, savedInstanceState)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    Timber.d("Player onViewCreated")
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    Timber.d("Player onDestroy")
+  }
+
+  override fun onDetach() {
+    super.onDetach()
+    playerView.removeFullScreenListener(fullScreenListener)
+    Timber.d("Player onDetach")
   }
 }
 //https://medium.com/@oshanm1/how-to-implement-a-search-dialog-using-full-screen-bottomsheetfragment-29ceb0af3d41
--- app/src/main/java/io/github/ovso/worship/view/ui/player/PlayerViewModel.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/player/PlayerViewModel.kt
@@ -1,8 +1,13 @@
 package io.github.ovso.worship.view.ui.player
 
+import androidx.databinding.ObservableBoolean
 import io.github.ovso.worship.view.base.DisposableViewModel
 
 class PlayerViewModel : DisposableViewModel() {
+  val isBookmarkSelected = ObservableBoolean(false)
+  var second = 0F
 
-  var second: Float = 0F
+  fun onBookmarkClick() {
+    isBookmarkSelected.set(isBookmarkSelected.get().not())
+  }
 }
