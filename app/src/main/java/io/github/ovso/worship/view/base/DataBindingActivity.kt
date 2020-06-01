56c6947026c1402013f0f7b19d6a21b9cc9a0780
jaeho <ovsoce@gmail.com>
2020-06-01 10:39:35 +0900

    Apply DataBinding on PlayerActivity

--- app/src/main/java/io/github/ovso/worship/view/base/DataBindingActivity.kt
+++ app/src/main/java/io/github/ovso/worship/view/base/DataBindingActivity.kt
@@ -6,13 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import io.github.ovso.worship.BR
 
 abstract class DataBindingActivity<T : ViewDataBinding>(
-  @LayoutRes private val layoutResId: Int,
-  private val viewModelCls: Class<out ViewModel>
+  @LayoutRes private val layoutResId: Int
 ) : AppCompatActivity() {
+
+  abstract val viewModel: ViewModel
 
   @Suppress("RemoveExplicitTypeArguments")
   protected val binding: T by lazy {
@@ -28,7 +28,7 @@ abstract class DataBindingActivity<T : ViewDataBinding>(
     val owner = this@DataBindingActivity
     with(binding) {
       lifecycleOwner = owner
-      setVariable(BR.viewModel, ViewModelProvider(owner)[viewModelCls])
+      setVariable(BR.viewModel, viewModel)
       executePendingBindings()
     }
   }
--- app/src/main/java/io/github/ovso/worship/view/ui/player/PlayerActivity.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/player/PlayerActivity.kt
@@ -1,19 +1,21 @@
 package io.github.ovso.worship.view.ui.player
 
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.utils.loadOrCueVideo
 import io.github.ovso.worship.R
+import io.github.ovso.worship.databinding.ActivityPlayerBinding
 import io.github.ovso.worship.extensions.getViewModelFactory
+import io.github.ovso.worship.view.base.DataBindingActivity
 import kotlinx.android.synthetic.main.view_player.*
 import timber.log.Timber
 
-class PlayerActivity : AppCompatActivity(R.layout.activity_player) {
+class PlayerActivity : DataBindingActivity<ActivityPlayerBinding>(R.layout.activity_player) {
 
-  private val viewModel by viewModels<PlayerViewModel> { getViewModelFactory() }
+//  private val viewModel by viewModels<PlayerViewModel> { getViewModelFactory() }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -21,6 +23,7 @@ class PlayerActivity : AppCompatActivity(R.layout.activity_player) {
   }
 
   private fun play() {
+/*
     intent.getStringExtra("videoId")?.let {
       ypv_player.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
         override fun onReady(youTubePlayer: YouTubePlayer) {
@@ -38,6 +41,13 @@ class PlayerActivity : AppCompatActivity(R.layout.activity_player) {
         }
       })
     }
+*/
   }
+
+  override val viewModel: ViewModel
+    get() = ViewModelProvider(
+      this@PlayerActivity,
+      getViewModelFactory()
+    )[PlayerViewModel::class.java]
 
 }
