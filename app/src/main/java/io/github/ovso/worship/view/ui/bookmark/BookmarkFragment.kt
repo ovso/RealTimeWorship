6616c14ec13239eaec32b6fd1fb02ad594bb3969
jaeho <ovsoce@gmail.com>
2020-06-03 14:56:51 +0900

    Replaced viewBinding, dataBinding to buildFeatures

--- app/src/main/java/io/github/ovso/worship/view/ui/bookmark/BookmarkFragment.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/bookmark/BookmarkFragment.kt
@@ -1,7 +1,15 @@
 package io.github.ovso.worship.view.ui.bookmark
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import io.github.ovso.worship.R
+import io.github.ovso.worship.extensions.toast
 
-class BookmarkFragment : Fragment() {
+class BookmarkFragment : Fragment(R.layout.fragment_bookmark) {
 
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    toast("onViewCreated")
+  }
 }
