439c3c302bfa0d97ea892a7d04d25191fd737eae
jaeho <ovsoce@gmail.com>
2020-08-22 06:56:55 +0900

    refactor: Modify adapter creation and reference

--- app/src/main/java/io/github/ovso/worship/view/ui/bookmark/BookmarkFragment.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/bookmark/BookmarkFragment.kt
@@ -5,9 +5,8 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.RecyclerView
 import io.github.ovso.worship.R
+import io.github.ovso.worship.extensions.defaultDivider
 import io.github.ovso.worship.extensions.getViewModelFactory
 import io.github.ovso.worship.view.ui.bookmark.adapter.BookmarkAdapter
 import kotlinx.android.synthetic.main.fragment_bookmark.*
@@ -15,8 +14,6 @@ import kotlinx.android.synthetic.main.fragment_bookmark.*
 class BookmarkFragment : Fragment(R.layout.fragment_bookmark) {
 
   private val viewModel: BookmarkViewModel by viewModels { getViewModelFactory() }
-
-  private val adapter by lazy { BookmarkAdapter() }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
@@ -26,12 +23,14 @@ class BookmarkFragment : Fragment(R.layout.fragment_bookmark) {
 
   private fun observe() {
     viewModel.items.observe(viewLifecycleOwner, Observer {
-      adapter.submitList(it)
+      (rv_bookmark.adapter as? BookmarkAdapter)?.submitList(it)
     })
   }
 
   private fun setupRv() {
-    rv_bookmark.adapter = adapter
-    rv_bookmark.addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+    with(rv_bookmark) {
+      defaultDivider()
+      adapter = BookmarkAdapter()
+    }
   }
 }
