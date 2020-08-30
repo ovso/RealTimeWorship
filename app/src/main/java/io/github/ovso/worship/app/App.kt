1a1c2f42956d409ad43e2e7057beb0b5e82b865c
jaeho <ovsoce@gmail.com>
2020-08-30 22:36:21 +0900

    feat: Add hilt on App.kt

--- app/src/main/java/io/github/ovso/worship/app/App.kt
+++ app/src/main/java/io/github/ovso/worship/app/App.kt
@@ -2,10 +2,12 @@ package io.github.ovso.worship.app
 
 import android.app.Application
 import androidx.room.Room
+import dagger.hilt.android.HiltAndroidApp
 import io.github.ovso.worship.data.TasksRepository
 import io.github.ovso.worship.data.local.AppDatabase
 import io.github.ovso.worship.data.remote.ServiceLocator
 
+@HiltAndroidApp
 class App : Application() {
 
   val taskRepository: TasksRepository
