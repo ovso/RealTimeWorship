baa0320cb749e75fe5473f4cf67562a2f34e9a1d
jaeho <ovsoce@gmail.com>
2020-09-08 08:06:01 +0900

    test: Replace video to video2 interface

--- app/src/main/java/io/github/ovso/worship/data/remote/TasksRemoteDataSource.kt
+++ app/src/main/java/io/github/ovso/worship/data/remote/TasksRemoteDataSource.kt
@@ -3,7 +3,6 @@ package io.github.ovso.worship.data.remote
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonElement
-import com.google.gson.JsonObject
 import io.github.ovso.worship.data.remote.response.VideoResponse
 import io.github.ovso.worship.data.toVideoResponses
 import io.github.ovso.worship.extensions.fromJson
