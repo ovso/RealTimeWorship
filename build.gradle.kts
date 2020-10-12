4368fb1d4b832195835de26f0c5a9451e50261db
jaeho <ovsoce@gmail.com>
2020-10-12 21:17:27 +0900

    feat: Add unit id, Grdale versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
   dependencies {
     classpath("com.android.tools.build:gradle:${Versions.gradle}")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
-    classpath("com.google.gms:google-services:4.3.3")
+    classpath("com.google.gms:google-services:4.3.4")
     classpath("com.google.android.gms:oss-licenses-plugin:0.10.2")
     classpath("com.google.dagger:hilt-android-gradle-plugin:2.28.1-alpha")
   }
