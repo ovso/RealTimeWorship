d78ab1b1d0809bc673918b80b7ce1ee27d42fd14
jaeho <ovsoce@gmail.com>
2020-10-17 01:15:06 +0900

    feat: Implement circular progress

--- commons/view/src/main/java/io/github/ovso/view/NativeAdsDialog.kt
+++ commons/view/src/main/java/io/github/ovso/view/NativeAdsDialog.kt
@@ -4,29 +4,35 @@ import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import androidx.core.view.isVisible
 import com.google.android.gms.ads.AdLoader
 import com.google.android.gms.ads.AdRequest
+import io.github.ovso.view.databinding.DialogNativeAds2Binding
 import io.github.ovso.view.databinding.DialogNativeAdsBinding
 
 class NativeAdsDialog2(context: Context) : AlertDialog.Builder(context) {
 
   private val binding by lazy {
-    DialogNativeAdsBinding.inflate(LayoutInflater.from(context))
+    DialogNativeAds2Binding.inflate(LayoutInflater.from(context))
   }
 
   private var unitId: String? = null
   fun setUnitId(unitId: String?): AlertDialog.Builder {
     this.unitId = unitId
-    val templateView = binding.root
+    setView(binding.root)
+//    binding.root.isRefreshing = true
+    binding.progressBar.isVisible = true
+    val templateView = binding.templateView
     val builder = AdLoader.Builder(context, unitId).apply {
       forUnifiedNativeAd {
         templateView.setNativeAd(it)
+        binding.progressBar.isVisible = false
       }
     }
     val adLoader = builder.build()
     val adRequest = AdRequest.Builder().build()
     adLoader.loadAd(adRequest)
-    return setView(binding.root)
+    return this
   }
 
   override fun setView(view: View?): AlertDialog.Builder {
