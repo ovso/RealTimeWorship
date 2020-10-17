6d07a65a39dabf25776e3f5f0c15d6f2cedd3f0f
jaeho <ovsoce@gmail.com>
2020-10-17 09:11:22 +0900

    feat: Implement NativeAdsDialog on HomeFragment.kt

--- app/src/main/java/io/github/ovso/worship/view/ui/home/HomeFragment.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/home/HomeFragment.kt
@@ -1,9 +1,12 @@
 package io.github.ovso.worship.view.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import io.github.ovso.view.NativeAdsDialog2
 import io.github.ovso.worship.R
 import io.github.ovso.worship.databinding.FragmentHomeBinding
 import io.github.ovso.worship.extensions.defaultDivider
@@ -18,6 +21,8 @@ class HomeFragment : DataBindingFragment<FragmentHomeBinding>(R.layout.fragment_
 
   @Inject
   lateinit var adapter: HomeAdapter
+
+  private lateinit var callback: OnBackPressedCallback
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
@@ -36,8 +41,30 @@ class HomeFragment : DataBindingFragment<FragmentHomeBinding>(R.layout.fragment_
     binding.rvHome.adapter = adapter
   }
 
+  override fun onAttach(context: Context) {
+    super.onAttach(context)
+    callback = object : OnBackPressedCallback(true) {
+      override fun handleOnBackPressed() {
+        NativeAdsDialog2(context)
+          .setUnitId(getString(R.string.ads_native_unit_id))
+          .setTitle(R.string.dialog_title_exit)
+          .setPositiveButton(R.string.dialog_exit_yes_btn) { dialog, _ ->
+            requireActivity().finish()
+          }.setNeutralButton(R.string.dialog_exit_no_btn) { dialog, _ ->
+            dialog.dismiss()
+          }.show()
+      }
+    }
+    requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+  }
+
   override fun onResume() {
     super.onResume()
     showBottomNav()
+  }
+
+  override fun onDetach() {
+    super.onDetach()
+    callback.remove()
   }
 }
--- app/src/main/java/io/github/ovso/worship/view/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/ovso/worship/view/ui/main/MainActivity.kt
@@ -1,21 +1,14 @@
 package io.github.ovso.worship.view.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.ads.nativetemplates.TemplateView
-import com.google.android.gms.ads.AdLoader
-import com.google.android.gms.ads.AdRequest
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
-import io.github.ovso.view.NativeAdsDialog2
 import io.github.ovso.worship.R
 import io.github.ovso.worship.extensions.loadAdaptiveBanner
 import kotlinx.android.synthetic.main.activity_main.*
@@ -41,44 +34,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     setupActionBarWithNavController(navController, appBarConfiguration)
     navView.setupWithNavController(navController)
     loadAdaptiveBanner(ads_container)
-
-    NativeAdsDialog2(this)
-      .setUnitId(getString(R.string.ads_native_unit_id))
-      .setTitle(R.string.dialog_title_exit)
-      .setPositiveButton(R.string.dialog_exit_yes_btn) { dialog, _ ->
-        dialog.dismiss()
-      }.setNeutralButton(R.string.dialog_exit_no_btn) { dialog, _ ->
-        dialog.dismiss()
-      }.show()
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
     onBackPressed()
     return super.onOptionsItemSelected(item)
-  }
-
-  fun adTest() {
-    Toast.makeText(this, "show dialog", Toast.LENGTH_SHORT).show()
-    val templateView =
-      LayoutInflater.from(this).inflate(R.layout.dialog_native_ads, null, false) as TemplateView
-    val builder = AdLoader.Builder(this, getString(R.string.ads_native_unit_id)).apply {
-      forUnifiedNativeAd {
-        templateView.setNativeAd(it)
-        Toast.makeText(this@MainActivity, "set native ad", Toast.LENGTH_SHORT).show()
-      }
-    }
-    val adLoader = builder.build()
-    val adRequest = AdRequest.Builder().build()
-    adLoader.loadAd(adRequest)
-    AlertDialog.Builder(this).setView(templateView)
-      .setTitle(R.string.dialog_title_exit)
-      .setPositiveButton(R.string.dialog_exit_yes_btn) { dialog, _ ->
-        dialog.dismiss()
-      }
-      .setNeutralButton(R.string.dialog_exit_no_btn) { dialog, _ ->
-        dialog.dismiss()
-      }
-      .show()
   }
 
 }
